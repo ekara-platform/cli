a908d1dcd93bb48ef856e9a554b520740eb29850
Adrien LAUER <adrien.lauer@gmail.com>
2019-05-13 12:47:02 +0200

    Fix copydep on unix

--- copydep.sh
+++ copydep.sh
@@ -1,5 +1,6 @@
 # Refresh "model"
 rm -rf ./vendor/github.com/ekara-platform/model/*.go
+mkdir -p ./vendor/github.com/ekara-platform/model/
 cp ../model/*.go  ./vendor/github.com/ekara-platform/model/
 
 
@@ -8,30 +9,30 @@ rm -rf ./vendor/github.com/ekara-platform/engine/*.go
 cp ../engine/*.go  ./vendor/github.com/ekara-platform/engine/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/ansible/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/ansible/
+mkdir -p ./vendor/github.com/ekara-platform/engine/ansible/
 cp ../engine/ansible/*.go  ./vendor/github.com/ekara-platform/engine/ansible/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/component/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/component/
+mkdir -p ./vendor/github.com/ekara-platform/engine/component/
 cp ../engine/component/*.go  ./vendor/github.com/ekara-platform/engine/component/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/ssh/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/ssh/
+mkdir -p ./vendor/github.com/ekara-platform/engine/ssh/
 cp ../engine/ssh/*.go  ./vendor/github.com/ekara-platform/engine/ssh/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/util/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/util/
+mkdir -p ./vendor/github.com/ekara-platform/engine/util/
 cp ../engine/util/*.go  ./vendor/github.com/ekara-platform/engine/util/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/component/scm/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/component/scm/
+mkdir -p ./vendor/github.com/ekara-platform/engine/component/scm/
 cp ../engine/component/scm/*.go  ./vendor/github.com/ekara-platform/engine/component/scm/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/component/scm/file/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/component/scm/file/
+mkdir -p ./vendor/github.com/ekara-platform/engine/component/scm/file/
 cp ../engine/component/scm/file/*.go  ./vendor/github.com/ekara-platform/engine/component/scm/file/
 
 rm -rf ./vendor/github.com/ekara-platform/engine/component/scm/git/*.go
-mkdir ./vendor/github.com/ekara-platform/engine/component/scm/git/
+mkdir -p ./vendor/github.com/ekara-platform/engine/component/scm/git/
 cp ../engine/component/scm/git/*.go  ./vendor/github.com/ekara-platform/engine/component/scm/git/
 
